2bee7a6be580dc9356f3fdbae60a69c6abd290f9
EtienneMT <emartor534@iesmartinezm.es>
2024-02-12 15:04:27 +0100

    Calculadora añadida

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -9,16 +9,40 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './calculadora.component.css',
 })
 export class CalculadoraComponent {
-  @Input()numero1: number = 0;
-  @Input()numero2: number = 0;
-  resultado: number = 0;
+  private _resultado: number = 0;
+  private _numero1: number = 0;
+  private _numero2: number = 0;
 
-  sumar() {
-    this.resultado = this.numero1 + this.numero2;
+  public get resultado(): number {
+    return this._resultado;
   }
 
-  restar() {
+  public set resultado(value: number) {
+    this._resultado = value;
+  }
+
+  
+  public get numero1(): number {
+    return this._numero1;
+  }
+  
+  public set numero1(value: number) {
+    this._numero1 = value;
+  }
+  
+  public get numero2(): number {
+    return this._numero2;
+  }
+
+  public set numero2(value: number) {
+    this._numero2 = value;
+  }
+
+  public sumar(): void {
+    this.resultado = Number(this.numero1) + Number(this.numero2);
+  }
+
+  public restar(): void {
     this.resultado = this.numero1 - this.numero2;
   }
-
 }
